105b9791cfe83a3af8f6eb130908e09c1dfe342b
TeknoSenpai <74503731+TeknoSenpai@users.noreply.github.com>
2022-06-02 13:49:38 +0200

    Updates

--- profile/index.js
+++
@@ -1,8 +0,0 @@
-const avatar = 'https://cdn.discordapp.com/avatars/836663939609657385/5ae58d36732d78c18878080e4b1ab24e';
-
-(function() {
-    /* Dynamic Avatar */
-    document.getElementById("dyn-avatar").setAttribute("src", avatar)
-    /* Static Avatar */
-    document.getElementById("st-avatar").setAttribute("href", avatar + '.png')
-})()
